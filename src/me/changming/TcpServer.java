c22a6d09274739979e682e6b804d38be973fa65f
changming.jiang <394358458@qq.com>
2017-10-26 15:44:57 +0800

    update

--- src/me/changming/TcpServer.java
+++ src/me/changming/TcpServer.java
@@ -1,25 +1,32 @@
 package me.changming;
 
+import me.changming.httpThread.HttpRootAcceptor;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import me.changming.httpThread.HttpRootAcceptor;
-
 public class TcpServer {
-    public static void main(String[] args) {
-        try {
-            ServerSocket serverSocket = new ServerSocket(8080);
-            Socket socket = serverSocket.accept();
+	public static void main( String[] args ) {
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket( 8080 );
+			Socket socket = serverSocket.accept();
 
-            while (socket != null) {
-                new HttpRootAcceptor(socket).start();
-                socket = serverSocket.accept();
-            }
-
-            serverSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			while ( socket != null ) {
+				new HttpRootAcceptor( socket ).start();
+				socket = serverSocket.accept();
+			}
+		} catch ( IOException e ) {
+			e.printStackTrace();
+		} finally {
+			if ( serverSocket != null ) {
+				try {
+					serverSocket.close();
+				} catch ( IOException e ) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
--- src/me/changming/httpThread/HttpRootAcceptor.java
+++ src/me/changming/httpThread/HttpRootAcceptor.java
@@ -1,39 +1,55 @@
 package me.changming.httpThread;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class HttpRootAcceptor extends Thread {
-    Socket socket;
+	private Socket socket;
 
-    public HttpRootAcceptor(Socket socket) {
-        this.socket = socket;
-    }
+	public HttpRootAcceptor( Socket socket ) {
+		this.socket = socket;
+	}
 
-    @Override
-    public void run() {
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	@Override
+	public void run() {
+		BufferedReader bufferedReader = null;
+		BufferedWriter bufferedWriter = null;
+		try {
+			bufferedReader = new BufferedReader( new InputStreamReader( socket.getInputStream() ) );
 
-            String str = null;
-            while ((str = bufferedReader.readLine()) != null && !str.equals(""))
-                System.out.println(str);
+			String str = null;
+			while ( ( str = bufferedReader.readLine() ) != null && !str.equals( "" ) )
+				System.out.println( str );
 
-            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            bufferedWriter.write("HTTP/1.1 200 OK\n");
-            bufferedWriter.write("Content-Type: application/json; charset=UTF-8\n\n");
-            bufferedWriter.write("{\"a\": \"Hello\", \"b\": \"World\"}");
-            bufferedWriter.flush();
-            bufferedWriter.close();
-
-            bufferedReader.close();
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			bufferedWriter = new BufferedWriter( new OutputStreamWriter( socket.getOutputStream() ) );
+			bufferedWriter.write( "HTTP/1.1 200 OK\n" );
+			bufferedWriter.write( "Content-Type: application/json; charset=UTF-8\n\n" );
+			bufferedWriter.write( "{\"a\": \"Hello\", \"b\": \"World\"}" );
+		} catch ( IOException e ) {
+			e.printStackTrace();
+		} finally {
+			if ( bufferedReader != null ) {
+				try {
+					bufferedReader.close();
+				} catch ( IOException e ) {
+					e.printStackTrace();
+				}
+			}
+			if ( bufferedWriter != null ) {
+				try {
+					bufferedWriter.flush();
+					bufferedWriter.close();
+				} catch ( IOException e ) {
+					e.printStackTrace();
+				}
+			}
+			if ( socket != null ) {
+				try {
+					socket.close();
+				} catch ( IOException e ) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
